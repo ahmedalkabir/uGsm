c39440639837e8d45df8b3e93e4a97d1507e7b5c
Ahmed Alkabir <korabekaali@outlook.com>
2019-11-26 17:25:50 +0200

    another weird implementation of doCommand but it works

--- ugsm.h
+++ ugsm.h
@@ -10,7 +10,7 @@
 class uGsm
 {
 public:
-  uGsm(){}
+  uGsm() {}
   void begin(Stream *_serial);
 
   // to check if the arduino is connected to module
@@ -22,10 +22,10 @@ public:
   // to send message to specifc destenation
   bool sendSMS(const char *dst, const char *msg);
   bool sendSMS(const __FlashStringHelper *dst, const __FlashStringHelper *msg);
-  
+
   // return true if there's income message to read
   bool messageToRead();
-  
+
   // return 1 if readSMS read message successfully otherwise return 0, and return -1
   // for invalid input
   int readLastSMS(char *phone_number, char *received_message);
@@ -34,11 +34,11 @@ public:
   // return 1 if readSMS read message successfully otherwise return 0, and return -1
   // for invalid input
   int readSMS(uint8_t index_m, char *phone_number, char *received_message);
-  // doCommand : this function is going execute callback function based 
+  // doCommand : this function is going execute callback function based
   // on received command and must be used inside if(object.messageToRead){}
   void doCommand(const char *cmd, void (*cb)());
   void doCommand(const __FlashStringHelper *cmd, void (*cb)());
-  
+
   bool deleteSMS(uint8_t index_m);
   bool deleteSMS(const char *index_m);
   // is used to delete all messages stored in sms
@@ -59,53 +59,63 @@ private:
   void flush_the_serial_and_buffer();
   bool wait_for_response(const char *rsp, uint16_t time_out);
   bool wait_for_response(const __FlashStringHelper *rsp, uint16_t time_out);
-  char* read_buffer();
+  char *read_buffer();
   inline bool is_contain_response(const char *rsp);
   inline bool is_contain_response(const __FlashStringHelper *rsp);
 };
 
 // implementation of private methods
-char* uGsm::read_buffer(){
-  if(_serialGSM->available() > 0){
+char *uGsm::read_buffer()
+{
+  if (_serialGSM->available() > 0)
+  {
     char ch;
     char *ptr = buffer;
-    while(_serialGSM->available() > 0){
+    while (_serialGSM->available() > 0)
+    {
       ch = _serialGSM->read();
       *ptr++ = ch;
     }
     *ptr = '\0';
-  } 
+  }
   // let's make a few delay between command
   // Serial.println(buffer);
   delay(20);
   return buffer;
 }
 
-void uGsm::write_at_command(const char *cmd){
+void uGsm::write_at_command(const char *cmd)
+{
   flush_the_serial_and_buffer();
   uint8_t cmd_len = strlen(cmd);
-  for(uint8_t i = 0; i < cmd_len; i++){
+  for (uint8_t i = 0; i < cmd_len; i++)
+  {
     _serialGSM->write(cmd[i]);
   }
   delay(500);
 }
 
-void uGsm::write_at_command(const __FlashStringHelper *cmd){
+void uGsm::write_at_command(const __FlashStringHelper *cmd)
+{
   char cmdR[strlen_P(reinterpret_cast<const char *>(cmd)) + 1];
   strcpy_P(cmdR, reinterpret_cast<const char *>(cmd));
   write_at_command(cmdR);
 }
 
-bool uGsm::wait_for_response(const char *response, uint16_t time_out = 1000){
+bool uGsm::wait_for_response(const char *response, uint16_t time_out = 1000)
+{
   uint16_t last_time = millis();
-  while(1){
+  while (1)
+  {
     const char *pBuffer = read_buffer();
-    if(is_contain_response(response)){
+    if (is_contain_response(response))
+    {
       flush_the_serial_and_buffer();
       return true;
     }
 
-    if(millis() - last_time > time_out){
+    if (millis() - last_time > time_out)
+    {
       flush_the_serial_and_buffer();
       return false;
     }
@@ -113,61 +123,71 @@ bool uGsm::wait_for_response(const char *response, uint16_t time_out = 1000){
   return false;
 }
 
-bool uGsm::wait_for_response(const __FlashStringHelper *response, uint16_t time_out){
+bool uGsm::wait_for_response(const __FlashStringHelper *response, uint16_t time_out)
+{
   char responseR[strlen_P(reinterpret_cast<const char *>(response)) + 1];
   strcpy_P(responseR, reinterpret_cast<const char *>(response));
   return wait_for_response(responseR, time_out);
 }
 
-void uGsm::flush_the_serial_and_buffer(){
-  while(_serialGSM->available() > 0)
+void uGsm::flush_the_serial_and_buffer()
+{
+  while (_serialGSM->available() > 0)
     _serialGSM->read();
   memset(buffer, '\0', 203);
 }
 
-bool uGsm::is_contain_response(const char *rsp){
-  if(strstr(buffer, rsp) == NULL)
+bool uGsm::is_contain_response(const char *rsp)
+{
+  if (strstr(buffer, rsp) == NULL)
     return false;
   return true;
 }
 
-bool uGsm::is_contain_response(const __FlashStringHelper *rsp){
+bool uGsm::is_contain_response(const __FlashStringHelper *rsp)
+{
   char rsp_r[strlen_P(reinterpret_cast<const char *>(rsp)) + 1];
   strcpy_P(rsp_r, reinterpret_cast<const char *>(rsp));
   return is_contain_response(rsp_r);
 }
 
 // implementation of public methods
-void uGsm::begin(Stream *_serial){
+void uGsm::begin(Stream *_serial)
+{
   _serialGSM = _serial;
   enableEcho();
 }
 
-void uGsm::disableEcho(){
+void uGsm::disableEcho()
+{
   write_at_command(F("ATE0\r"));
   flush_the_serial_and_buffer();
 }
 
-void uGsm::enableEcho(){
+void uGsm::enableEcho()
+{
   write_at_command(F("ATE1\r"));
   flush_the_serial_and_buffer();
 }
 
-bool uGsm::isPoweredUp(uint16_t time_out){
+bool uGsm::isPoweredUp(uint16_t time_out)
+{
   write_at_command(F("AT\r"));
   return wait_for_response(F("OK"), time_out);
 }
 
-bool uGsm::isRegistered(uint16_t time_out){
+bool uGsm::isRegistered(uint16_t time_out)
+{
   write_at_command(F("AT+COPS?\r"));
   return wait_for_response(F("+COPS: 0,0"), time_out);
 }
 
-bool uGsm::sendSMS(const char *dst, const char *msg){
+bool uGsm::sendSMS(const char *dst, const char *msg)
+{
   // let's set text mode for sending message
   flush_the_serial_and_buffer();
   write_at_command(F("AT+CMGF=1\r"));
-  if(!wait_for_response(F("OK"), 3000))
+  if (!wait_for_response(F("OK"), 3000))
     return false;
   delay(20);
   // let's send message
@@ -175,12 +195,14 @@ bool uGsm::sendSMS(const char *dst, const char *msg){
   sprintf_P(sms_dst, PSTR("AT+CMGS=\"%s\"\r"), dst);
   write_at_command(sms_dst);
   delay(20);
-  if(wait_for_response(F(">"), 3000)){
+  if (wait_for_response(F(">"), 3000))
+  {
     // DEBUG
     // Serial.println("DEBUG -- SEND MESSAGE");
-    // write message 
+    // write message
     const char *pChar = msg;
-    while(*pChar != '\0'){
+    while (*pChar != '\0')
+    {
       _serialGSM->write(*pChar++);
     }
 
@@ -188,14 +210,18 @@ bool uGsm::sendSMS(const char *dst, const char *msg){
     _serialGSM->write((char)26);
     delay(200);
     flush_the_serial_and_buffer();
-    while(_serialGSM->available() == 0);
+    while (_serialGSM->available() == 0)
+      ;
     return wait_for_response(F("+CMG"), 5000);
-  }else{
+  }
+  else
+  {
     return false;
   }
 }
 
-bool uGsm::sendSMS(const __FlashStringHelper *dst, const __FlashStringHelper *msg){
+bool uGsm::sendSMS(const __FlashStringHelper *dst, const __FlashStringHelper *msg)
+{
   char dst_r[strlen_P(reinterpret_cast<const char *>(dst)) + 1];
   char msg_r[strlen_P(reinterpret_cast<const char *>(msg)) + 1];
 
@@ -205,8 +231,10 @@ bool uGsm::sendSMS(const __FlashStringHelper *dst, const __FlashStringHelper *ms
   return sendSMS(dst_r, msg_r);
 }
 
-bool uGsm::messageToRead(){
-  if(_serialGSM->available() > 0){
+bool uGsm::messageToRead()
+{
+  if (_serialGSM->available() > 0)
+  {
     memset(last_message_index, '\0', 3);
     const char *expct_rsp = "+CMTI: \"SM\",";
     const char *pBuffer = read_buffer();
@@ -214,11 +242,14 @@ bool uGsm::messageToRead(){
     uint8_t incrChar = 0;
 
     char *pLastIndex = last_message_index;
-    while(*pBuffer != '\0'){
+    while (*pBuffer != '\0')
+    {
       (*pBuffer++ == expct_rsp[incrChar]) ? incrChar++ : 0;
-      if(incrChar == len_rsp){
+      if (incrChar == len_rsp)
+      {
         // here i'm going to read the index of received message
-        for(uint8_t i=0; i < strlen(pBuffer); i++){
+        for (uint8_t i = 0; i < strlen(pBuffer); i++)
+        {
           *pLastIndex++ = *pBuffer++;
         }
         *pLastIndex = '\0';
@@ -231,16 +262,18 @@ bool uGsm::messageToRead(){
   return false;
 }
 
-int uGsm::readLastSMS(char *phone_number, char *received_message){
+int uGsm::readLastSMS(char *phone_number, char *received_message)
+{
   return readSMS(atoi(last_message_index), phone_number, received_message);
 }
 
-int uGsm::readSMS(uint8_t index_m, char *phone_number, char *received_message){
+int uGsm::readSMS(uint8_t index_m, char *phone_number, char *received_message)
+{
   char c_index_m[3];
   char cmd[12];
   itoa(index_m, c_index_m, 10);
   // uint16_t last_time = millis();
-  sprintf_P(cmd,PSTR("AT+CMGR=%s\r"), c_index_m);
+  sprintf_P(cmd, PSTR("AT+CMGR=%s\r"), c_index_m);
   flush_the_serial_and_buffer();
   write_at_command(cmd);
   char *pBuffer = read_buffer();
@@ -249,40 +282,46 @@ int uGsm::readSMS(uint8_t index_m, char *phone_number, char *received_message){
   char *pPhone_number = phone_number;
   char *pReceived_message = received_message;
   bool phoneFetchedDone = false;
-  while(*pBuffer != '\0'){
+  while (*pBuffer != '\0')
+  {
     // char ch = *pBuffer++;
     // start reading phone number
-    if(*pBuffer++ == ',' && !phoneFetchedDone){
+    if (*pBuffer++ == ',' && !phoneFetchedDone)
+    {
       // ignore "
       *pBuffer++;
-      while(*pBuffer != '"'){
+      while (*pBuffer != '"')
+      {
         *pPhone_number++ = *pBuffer++;
       }
       *pPhone_number = '\0';
       phoneFetchedDone = true;
     }
     // let's wait for <CR>
-    if(*pBuffer == '\r' && phoneFetchedDone){
+    if (*pBuffer == '\r' && phoneFetchedDone)
+    {
       // ignore <LF>
       *pBuffer++;
       *pBuffer++;
-      while(*pBuffer != '\0'){
+      while (*pBuffer != '\0')
+      {
         // Serial.write(*pBuffer++);
         *pReceived_message++ = *pBuffer++;
         // break current loop
-        if(*pBuffer == '\r')
+        if (*pBuffer == '\r')
           break;
       }
-      // add the null character to state it as string literals 
+      // add the null character to state it as string literals
       *pReceived_message = '\0';
-      // after we got the contents of message we stop the loop 
+      // after we got the contents of message we stop the loop
       return 1;
     }
   }
   return 0;
 }
 
-void uGsm::doCommand(const char *cmd, void (*cb)()){
+void uGsm::doCommand(const char *cmd, void (*cb)())
+{
   // well, we're going to read last message as suppoesd to be a command to do something special
   flush_the_serial_and_buffer();
   char cmd_at[10];
@@ -291,50 +330,69 @@ void uGsm::doCommand(const char *cmd, void (*cb)()){
   write_at_command(cmd_at);
   char *pBuffer = read_buffer();
   char *cmd_msg;
-  while(*pBuffer != '\0'){
-    // something weird happens here, and I may need to reconsider 
+  while (*pBuffer != '\0')
+  {
+    // something weird happens here, and I may need to reconsider
     // the implementation
-    if(*pBuffer++ == '\r' && incN++ == 3){
+    if (*pBuffer++ == '\r' && incN++ == 2)
+    {
+      incN=1;
       *pBuffer++;
-      cmd_msg = pBuffer;
-      break;
+      while(*pBuffer != '\0'){
+        if(*pBuffer++ == '"' && incN++ == 8){
+          *pBuffer++;
+          *pBuffer++;
+          cmd_msg = pBuffer;
+          break;
+        }
+      }
+      while (*pBuffer != '\0')
+      {
+        if (*pBuffer++ == '\r')
+        {
+          *--pBuffer = '\0';
+          break;
+        }
+      }
     }
   }
-  while(*pBuffer != '\0'){
-    if(*pBuffer++ == '\r'){
-      *--pBuffer = '\0';
-      break;
-    }
-  }
+
+  // Serial.println(cmd_msg);
+  // Serial.print(pBuffer);
   // compare command to received message and invoke callback incase are same
-  if(strcmp(cmd_msg, cmd) == 0){
+  if (strcmp(cmd_msg, cmd) == 0)
+  {
     cb();
   }
   flush_the_serial_and_buffer();
-  // now let's delete the current message to prevent from filling the storage of sms 
+  // now let's delete the current message to prevent from filling the storage of sms
   // deleteSMS(last_message_index);
 }
 
-void uGsm::doCommand(const __FlashStringHelper *cmd, void (*cb)()){
+void uGsm::doCommand(const __FlashStringHelper *cmd, void (*cb)())
+{
   char cmd_d[strlen_P(reinterpret_cast<const char *>(cmd)) + 1];
   strcpy_P(cmd_d, reinterpret_cast<const char *>(cmd));
-  
+
   doCommand(cmd_d, cb);
 }
 
-bool uGsm::deleteSMS(const char *index_m){
+bool uGsm::deleteSMS(const char *index_m)
+{
   char cmd[10];
   sprintf_P(cmd, PSTR("AT+CMGD=%s\r"), index_m);
   write_at_command(cmd);
   return wait_for_response(F("OK"), 3000);
 }
 
-bool uGsm::deleteAllSMS(){
+bool uGsm::deleteAllSMS()
+{
   write_at_command(F("AT+CMGD=0,4\r"));
   return wait_for_response(F("OK"), 3000);
 }
 
-void uGsm::test_responed_function(){
+void uGsm::test_responed_function()
+{
   write_at_command("AT+COPS?\r");
   Serial.println(read_buffer());
 }
